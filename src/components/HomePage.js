cdbbbb6781883d26a030dc847455365866012cd7
briecodes <hello@brie.work>
2018-07-03 16:30:00 -0400

    attending list defined.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -20,26 +20,22 @@ class HomePage extends React.Component {
       array.forEach(event => {
         if (event.user_id === this.props.activeUser.id){
           if (event.active){
-            // console.log('active event', event);
             this.setState({
               myCurrentEvents: [...this.state.myCurrentEvents, event]
             });
           }else{
-            // console.log('else event', event);
             this.setState({
               myPastEvents: [...this.state.myPastEvents, event]
             });
           };
-
-          event.song_entries.forEach(song => {
-            if (song.user_id === this.props.activeUser.id){
-              this.setState({
-                visitedEvents: [...this.state.visitedEvents, event]
-              });
-            };
-          });
-
         };
+        event.user_events.forEach(usrEvt => {
+          if (usrEvt.user_id === this.props.activeUser.id){
+            this.setState({
+              visitedEvents: [...this.state.visitedEvents, event]
+            });
+          };
+        });
       });
     });
   };
@@ -67,12 +63,8 @@ class HomePage extends React.Component {
         </ul>
         <ul>
           <h3>Attending Events:</h3>
-          {/* {this.state.myPastEvents.map(event => <li key={event.id} >{event.title} ></li>)} */}
+          {this.state.visitedEvents.map(event => <li key={event.id} ><Link to={'/events/' + event.id} onClick={() => this.exploreEvent(event)} >{event.title} ></Link></li>)}
         </ul>
-        {/* <ul>
-          <h3>Visited Events:</h3>
-          {this.state.visitedEvents.map(event => <li key={event.id} ><Link to={'/events/' + event.id} onClick={() => this.exploreEvent(event)} >{event.title} > </Link>{event.song_entries.map(song => <p key={song.id}>{song.song_title}</p>)}</li>)}
-        </ul> */}
       </div>
     );
   };
