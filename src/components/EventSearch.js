b249ec4da192348d63df39c5f563455cccb5bbb6
briecodes <hello@brie.work>
2018-07-03 23:22:39 -0400

    search functionality defined.

--- src/components/EventSearch.js
+++ src/components/EventSearch.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class EventSearch extends React.Component {
-    
+    state = {
+        searchTerm: '',
+        foundEvents: []
+    };
+
+    inputControl = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+        this.searchEvents();
+    };
+
+    fetchEvents = () => {
+        fetch('http://localhost:3000/api/v1/events').then( response => response.json() ).then(array => {
+            const foundEvents = array.filter(event => event.title.toLowerCase().includes(this.state.searchTerm.toLowerCase()) && event.user_id !== this.props.activeUser.id);
+            this.setState({
+                foundEvents
+            });
+        });
+    };
+
+    searchEvents = () => {
+        if (this.state.searchTerm === ''){
+            this.setState({
+                foundEvents: []
+            });
+        }else{
+            this.fetchEvents();
+        };
+    };
+
     render() {
         return (
             <div>
-                <input type='text' name='search_events' placeholder='Search Events' />
+                <input type='text' name='searchTerm' placeholder='Search Events' value={this.state.searchTerm} onChange={this.inputControl} />
+                <ul>
+                    {this.state.foundEvents.map(event => <li key={event.id}>{event.title}</li>)}
+                </ul>
             </div>
         );
     };
 };
 
-export default EventSearch;
+const mapStateToProps = (state) => {
+    return {
+      activeUser: state.activeUser
+    };
+  };
+
+export default connect(mapStateToProps)(EventSearch);
