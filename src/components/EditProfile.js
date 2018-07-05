ce0e2b36c00b28c46f0b18c2e96a49e16769b3b8
briecodes <hello@brie.work>
2018-07-05 16:52:49 -0400

    edit user form persists changes, fails to show up on front-end.

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { setUser } from '../actions/index';
+
 class EditProfile extends React.Component {
+  state = {
+    name: this.props.activeUser.name,
+    username: this.props.activeUser.username,
+    success: false
+  };
+
+  inputControl = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  save = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/api/v1/users/' + this.props.activeUser.id, {
+      method: 'PATCH',
+      body: JSON.stringify({username: this.state.username, name: this.state.name}),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then( res => res.json() )
+    .then( response => {
+      // console.log('response:', response);
+      this.props.dispatch(setUser(response));
+      this.setState({
+        success: true
+      });
+    });
+  };
+
   render() {
     return (
-      <form onSubmit={this.createUser}>
-        <h1>Edit Profile:</h1>
+      <form onSubmit={this.save}>
+        <h1>{this.props.activeUser.id}Edit {this.props.activeUser.name}'s Profile:</h1>
+        {this.state.success ? <h3>Changes Saved!</h3> : null }
         <input type='text' name='username' placeholder='Username' value={this.state.username} onChange={this.inputControl} />
         <input type='text' name='name' placeholder='Name' value={this.state.name} onChange={this.inputControl} />
-        <input type='password' name='password' placeholder='Password' value={this.state.password} onChange={this.inputControl} />
-        <input type='password' name='password_retype' placeholder='Password Confirmation' value={this.state.password_retype} onChange={this.checkPassword} />
         <input type='submit'/>
       </form>
     );
@@ -18,7 +48,7 @@ class EditProfile extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    userId: state.userId
+    activeUser: state.activeUser
   };
 };
 
