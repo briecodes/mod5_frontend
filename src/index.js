68dfac8e52d273a1455ec70c768f21c3bb1a5636
briecodes <hello@brie.work>
2018-07-06 12:10:03 -0400

    reducer refactored into switch method.

--- src/index.js
+++ src/index.js
@@ -19,36 +19,36 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'SET_USER'){
-    return {...state, activeUser: action.payload.user}
-  }else if (action.type === 'SET_USER_ID'){
-    return {...state, user_id: action.payload.user_id}
-  }else if (action.type === 'FAKED_USER'){
-    return {...state, activeUser: action.payload.user}
-  }else if (action.type === 'ACTIVE_EVENT'){
-    return {...state, activeEvent: action.payload.event}
-  }else if (action.type === 'LOGOUT'){
-    return {...state, activeUser: action.payload.user}
-  }else if (action.type === 'SET_LOCATION'){
-    return {...state, currentLocation: action.payload.currentLocation}
-  }else if (action.type === 'VIDEO_FORM_SELECTION'){
-    return {...state, video_id: action.payload.video_id, video_url: action.payload.video_url}
-  }else if (action.type === 'ADD_PERFORMER_TO_LIST'){
-    return {...state, performerList: [...state.performerList, action.payload.performer]}
-  }else if (action.type === 'LOAD_PERFORMERS'){
-    return {...state, performerList: action.payload.performerList}
-  }else if (action.type === 'RESET_STORE'){
-    return {...state, performerList: initialState.performerList, video_url: initialState.video_url, video_id: initialState.video_id}
-  }else if (action.type === 'RESET_APP'){
-    console.log('state:', state);
-    console.log('action:', action);
-    return { initialState }
-  }else if (action.type === 'RESET_ACTIVE_EVENT'){
-    return {...state, activeEvent: null}
-  }else if (action.type === 'SET_TOKEN'){
-    return {...state, token: action.payload.token}
+  switch (action.type) {
+    case 'SET_USER':
+      return {...state, activeUser: action.payload.user};
+    case 'SET_USER_ID':
+      return {...state, user_id: action.payload.user_id};
+    case 'ACTIVE_EVENT':
+      return {...state, activeEvent: action.payload.event};
+    case 'LOGOUT':
+      return {...state, activeUser: action.payload.user};
+    case 'SET_LOCATION':
+      return {...state, currentLocation: action.payload.currentLocation};
+    case 'VIDEO_FORM_SELECTION':
+      return {...state, video_id: action.payload.video_id, video_url: action.payload.video_url};
+    case 'ADD_PERFORMER_TO_LIST':
+      return {...state, performerList: [...state.performerList, action.payload.performer]};
+    case 'LOAD_PERFORMERS':
+      return {...state, performerList: action.payload.performerList};
+    case 'RESET_STORE':
+      return {...state, performerList: initialState.performerList, video_url: initialState.video_url, video_id: initialState.video_id};
+    case 'RESET_APP':
+      console.log('state:', state);
+      console.log('action:', action);
+      return { initialState };
+    case 'RESET_ACTIVE_EVENT':
+      return {...state, activeEvent: null};
+    case 'SET_TOKEN':
+      return {...state, token: action.payload.token};
+    default:
+      return state;
   }
-  return state;
 };
 
 const store = createStore(reducer);
