208d309f2a4d038a2f60a134f576b6fbd4653483
briecodes <hello@brie.work>
2018-07-06 11:55:01 -0400

    reducers added to proper index file.

--- src/components/YouTubeSearchResults.js
+++ src/components/YouTubeSearchResults.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { selectVideoInForm } from '../actions/index';
+import { selectVideoInForm } from '../reducers/index';
 
 class YouTubeSearchResults extends React.Component {
 
