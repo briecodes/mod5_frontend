307e5c808d46daada95947db59450dd59343c2e8
briecodes <hello@brie.work>
2018-07-10 13:51:44 -0400

    render performer list lifted.

--- src/components/Event.js
+++ src/components/Event.js
@@ -184,6 +184,15 @@ class Event extends React.Component {
     })
   }
 
+  renderPerformerList = () => this.props.performerList.map((perf, index) => {
+    return (
+      <li key={perf.id} className='user-performer'>
+        { perf.user.id === loggedInUserId() ? <span className='heavy red'>{perf.user.name}</span> : <span className='heavy'>{perf.user.name}</span>} up {index > 0 ? ('in ' + index * 3.5 + ' mins') : 'now' } {perf.user.id === loggedInUserId() ? <input type='button' value='Cancel' className='cancel' onClick={() => this.deletePerformer(perf.id)} /> : null}<br />
+        performing <em>{perf.song_title}</em> by {perf.song_artist}
+      </li>
+    )
+  });
+
   render() {
     return (
       <React.Fragment>
@@ -220,14 +229,7 @@ class Event extends React.Component {
             <div className='col-half float-left' style={{maxHeight: '400px', overflowY: 'auto'}}>
               <h3>Songlist:</h3>
               <ul>
-                {this.props.performerList.map((perf, index) => {
-                  return (
-                    <li key={perf.id} className='user-performer'>
-                      { perf.user.id === loggedInUserId() ? <span className='heavy red'>{perf.user.name}</span> : <span className='heavy'>{perf.user.name}</span>} up {index > 0 ? ('in ' + index * 3.5 + ' mins') : 'now' } {perf.user.id === loggedInUserId() ? <input type='button' value='Cancel' className='cancel' onClick={() => this.deletePerformer(perf.id)} /> : null}<br />
-                      performing <em>{perf.song_title}</em> by {perf.song_artist}
-                    </li>
-                  )
-                })}
+                {this.renderPerformerList()}
               </ul>
             </div>
             <div className='col-half float-right mobile-col'>
