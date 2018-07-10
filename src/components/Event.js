b5e079fc02c754fe585e25785ae2ec9000103c4d
briecodes <hello@brie.work>
2018-07-10 15:19:34 -0400

    removed defunct action from import.

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import SongForm from '../components/SongForm';
 import PerformerList from '../components/PerformerList';
 import { setActiveEvent, setPerformerList } from '../reducers/index';
-import { HURL, loggedInUserId, localToken, pathEventId, getPeformers } from '../actions/index';
+import { HURL, loggedInUserId, localToken, pathEventId } from '../actions/index';
 
 class Event extends React.Component {
 
