85c129753d750eff81b01782545db10edc991f28
briecodes <hello@brie.work>
2018-07-10 15:34:38 -0400

    converted footer from class to functional component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,10 @@ import { Link } from 'react-router-dom';
 
 import { setCurrentLocation } from '../actions/index';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div id='footer'>
-        &#123; made with love <Link to={'/octothots'} onClick={() => this.props.dispatch(setCurrentLocation('/octothots'))} >4 octothorpes</Link> &#125;
-      </div>
-    );
-  };
-};
+const Footer = () => (
+  <div id='footer'>
+    &#123; made with love <Link to={'/octothots'} onClick={() => this.props.dispatch(setCurrentLocation('/octothots'))} >4 octothorpes</Link> &#125;
+  </div>
+);
 
 export default connect()(Footer);
